9014684b1738bb481e6b8be21acfb83063c55013
Helio Loureiro <helio.loureiro@ericsson.com>
2018-11-07 14:33:56 +0100

    Changed echos by alert so it can be seen on syslog.
     - Sounds stupid, but makes easier to track.

--- python-fuse-mongodb/run_test.sh
+++ python-fuse-mongodb/run_test.sh
@@ -4,14 +4,28 @@ TARGETDIR=./mnt
 
 updir=$(readlink -f $TARGETDIR/..)
 
-echo "mounting $TARGETDIR using fuse-mongodb"
+alert() {
+ echo $@
+ logger fuse-mongodb-testing "$@"
+}
+
+alert "Mounting $TARGETDIR using fuse-mongodb."
 python3 ./fuse-mongodb.py $TARGETDIR
 
-echo "listing parent directory"
+alert "Listing parent directory."
 ls -al $updir
-echo "listing directory"
+alert "Listing directory."
 ls -al $TARGETDIR
-echo "umounting $TARGETDIR"
+if [ $? -eq 0 ]; then
+    last_file=$(ls -tr $TARGETDIR | grep -v mongodb | head -1)
+    alert "Getting file content: $TARGETDIR/$last_file"
+    cat $TARGETDIR/$last_file | head
+    random_file="$USER-testing-$RANDOM"
+    alert "Testing write: $random_file"
+    touch $TARGETDIR/$random_file
+fi
+sleep 1
+alert "umounting $TARGETDIR"
 sudo umount $TARGETDIR
 
 
